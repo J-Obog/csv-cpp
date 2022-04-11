9897bb30192b77985e3d2ba28975b24b3a3ca93d
JObog <jobogbaimhe@gmail.com>
2022-04-11 15:40:27 -0400

    adding csv row class

--- csv.hpp
+++ csv.hpp
@@ -4,7 +4,7 @@
 
 std::vector<std::string> split_commas(const std::string& s) {
     std::vector<std::string> v; 
-    std::string buf = "";
+    std::string buf; 
     int pos = 0; 
 
     while(pos < s.length()) {
@@ -19,4 +19,16 @@ std::vector<std::string> split_commas(const std::string& s) {
 }
 
 
+class CSVRow {
+    private:
+        std::vector<std::string> _vec;
+        std::string _raw;  
 
+    public:
+        CSVRow() = default; 
+        ~CSVRow() = default; 
+        CSVRow(const std::string& s) {
+            _vec = split_commas(s);
+            _raw = s;  
+        }
+}; 
