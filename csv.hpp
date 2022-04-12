39890fc2e6a65dc77c0d0883f52d8fa03c8b616f
JObog <jobogbaimhe@gmail.com>
2022-04-11 21:28:04 -0400

    adding CSV writer and reader class

--- csv.hpp
+++ csv.hpp
@@ -1,12 +1,16 @@
 #pragma once
 #include <vector>
+#include <unordered_map>
 #include <string>
 
-std::vector<std::string> split_str(const std::string& s, char delim) {
-    std::vector<std::string> v; 
+typedef std::unordered_map<std::string, int> header_map; 
+typedef std::vector<std::string> str_vec; 
+
+str_vec split_str(std::string& s, char delim) {
+    str_vec v; 
     std::string buf; 
 
-    for(const char& c: s) {
+    for(char& c: s) {
         if(c == delim) {
             if(!buf.empty())
                 v.push_back(buf);
@@ -19,17 +23,46 @@ std::vector<std::string> split_str(const std::string& s, char delim) {
     return v; 
 }
 
-
 class CSVRow {
     private:
-        std::vector<std::string> _vec;
-        std::string _raw;  
-
+        header_map* _hmap_ptr;  
+        str_vec _cols;
+        
     public:
         CSVRow() = default; 
         ~CSVRow() = default; 
-        CSVRow(const std::string& s) {
-            _vec = split_str(s, ',');
-            _raw = s;  
+        
+        CSVRow(std::string& s) {
+            _cols = split_str(s, ',');
+        }
+
+        str_vec get_cols() const {
+            return _cols; 
+        }
+}; 
+
+class CSV {
+    private:
+        header_map _hmap; 
+        std::vector<CSVRow> _rows; 
+
+        void _set_headers() {
+            if(!_rows.empty()) {
+                int count = 0; 
+                
+                for(std::string& s: _rows[0].get_cols()) 
+                    _hmap.insert({s, count++}); 
+            }
+        }
+
+    public:
+        CSV() = default; 
+        ~CSV() = default; 
+        
+        CSV(std::string& s) {
+            for(std::string& txt: split_str(s, '\n'))
+                _rows.push_back(CSVRow(txt));                 
+
+             _set_headers(); 
         }
 }; 
