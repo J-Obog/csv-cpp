6e663709083af858c567b09ec9d77752696c86fb
JObog <jobogbaimhe@gmail.com>
2022-04-11 20:12:15 -0400

    using range-based for loop in split_str

--- csv.hpp
+++ csv.hpp
@@ -5,17 +5,17 @@
 std::vector<std::string> split_str(const std::string& s, char delim) {
     std::vector<std::string> v; 
     std::string buf; 
-    int pos = 0; 
 
-    while(pos < s.length()) {
-        if(s[pos] == delim) {
-            if(!buf.empty()) 
+    for(const char& c: s) {
+        if(c == delim) {
+            if(!buf.empty())
                 v.push_back(buf);
             buf.clear(); 
         } else {
-            buf += s[pos++];
+            buf += c; 
         }
     }
+
     return v; 
 }
 
